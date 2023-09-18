8c8f1e4e6d9ca71c969a8c1e5ca1b3a00aedbe14
DimitrovskiKristijan <119842042+DimitrovskiKristijan@users.noreply.github.com>
2023-09-18 20:02:23 +0200

    PROGETTO FINITO

--- registrazione.php
+++ registrazione.php
@@ -10,14 +10,16 @@
 </head>
 <body>
 <header>
-    <h1>Registrazione Obbligatoria prima di poter chattare</h1>
+    <h1 style="color: lightgreen;">Registrazione obbligatoria prima di poter fare il LOGIN</h1>
 </header>
 <form action="registrazione2.php" method="POST">
-<h2>REGISTRATI</h2>
-    <label>Username:</label><input type="text" name="username" id="username" placeholder="inserire lo username"  required><br>   
-    <label>Password:</label><input type="password" name="pwd" id="pwd" placeholder="inserire una password a scelta"  required ><br>
-    <input type="submit" id="btnRegistrati" value="Registrati"><br>
-  <p>Hai un account?<a href="index.html">Login</a></p>
+<h2 style="padding: 10px; color: lightgreen; text-align: center; background-color: rgb(24, 92, 59); margin: 0; margin-bottom: 10%;">REGISTRATI</h2>
+<div class="contenitoreAccesso">
+    <b><label style="font-size: 14pt; font-style: bold; color: lightgreen;">Username:</label></b><input type="text" name="username" id="username" placeholder="inserire lo username"  required><br><br><br>  
+    <b><label style="font-size: 14pt; font-style: bold; color: lightgreen;">Password:</label></b><input type="password" name="pwd" id="pwd" placeholder="inserire una password a scelta"  required ><br><br><br><br>
+    <input type="submit" id="btnRegistrati" value="Registrati"><br><br><br>
+  <p style="color: rgb(24, 92, 59);">Hai un account?<a href="index.html">Login</a></p>
+</div>
 </form>
 <footer>
 
